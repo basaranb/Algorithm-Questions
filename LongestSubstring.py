4c2b617d8da45d8d2e6062f7915614ba60d261ef
basaranb <basaranbadur@gmail.com>
2018-12-02 00:04:46 +0200

    added question

--- LongestSubstring.py
+++ LongestSubstring.py
@@ -1,3 +1,10 @@
+# This problem was asked by Amazon.
+
+# Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
+
+# For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+
+
 def find_longest(k,ar):
     length=0
     for start in range(len(ar)):
